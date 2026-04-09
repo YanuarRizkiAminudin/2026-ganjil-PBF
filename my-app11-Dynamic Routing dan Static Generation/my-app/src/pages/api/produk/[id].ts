023ef0e21fdbefcbcb297d4a8ba1e74ea8a93a66
Yanuar Rizki Aminudin <your-email@example.com>
2026-04-09 22:47:08 +0700

    Uji Perubahan Data

--- my-app11-Dynamic Routing dan Static Generation/my-app/src/pages/api/produk/[id].ts
+++ my-app11-Dynamic Routing dan Static Generation/my-app/src/pages/api/produk/[id].ts
@@ -5,6 +5,7 @@ const products: ProductType[] = [
   { id: '1', name: 'Sepatu Duramo SL', price: '900000', category: "Men's Shoes", size: '42', image: 'https://images.unsplash.com/photo-1542291026-7eec264c27ff?w=400' },
   { id: '2', name: 'SEPATU SAMBA OG', price: '2000000', category: "Men's Shoes", size: '41', image: 'https://images.unsplash.com/photo-1608231387042-66d1773070a5?w=400' },
   { id: '3', name: 'Sepatu Duramo SL (Ukuran 40)', price: '900.000', category: "Men's Shoes", size: '40', image: 'https://images.unsplash.com/photo-1542291026-7eec264c27ff?w=400' },
+  { id: '4', name: 'Sepatu Baru Test', price: '750000', category: "Men's Shoes", size: '43', image: 'https://images.unsplash.com/photo-1606107557195-0e29a4b5b4aa?w=400',}
 ];
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {

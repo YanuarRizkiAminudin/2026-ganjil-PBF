d9e07ccd73f039e802d324e7a36f25b48d78f3a2
Yanuar Rizki Aminudin <your-email@example.com>
2026-04-09 11:15:55 +0700

    Server Side Rendering (SSR)

---
+++ my-app9-Server Side Rendering SSR/my-app/src/pages/profile/index.tsx
@@ -0,0 +1,10 @@
+const ProfilePage = () => {
+  return (
+    <div>
+      <h1>Halaman Profile</h1>
+      <p>Ini adalah halaman profile pengguna.</p>
+    </div>
+  );
+};
+
+export default ProfilePage;

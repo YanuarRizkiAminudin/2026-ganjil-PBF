6428a54e8bd3d4925147af5eb9f53c8ceb8d5b11
Yanuar Rizki Aminudin <your-email@example.com>
2026-04-09 23:11:58 +0700

    Implementasikan halaman detail dengan

--- my-app11-Dynamic Routing dan Static Generation/my-app/src/pages/index.tsx
+++ my-app11-Dynamic Routing dan Static Generation/my-app/src/pages/index.tsx
@@ -1,12 +1,14 @@
-// src/pages/index.tsx
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div>
+    <div style={{ padding: '2rem', textAlign: 'center' }}>
       <h1>HOME</h1>
-      <a href="/produk">Ke Produk (CSR)</a><br />
-      <a href="/produk/server">Ke Produk (SSR)</a><br />
-      <a href="/produk/static">Ke Produk (SSG)</a>
+      <nav style={{ display: 'flex', gap: '1rem', justifyContent: 'center', marginTop: '2rem' }}>
+        <Link href="/produk">Ke Produk (CSR)</Link>
+        <Link href="/produk/server">Ke Produk (SSR)</Link>
+        <Link href="/produk/static">Ke Produk (SSG)</Link>
+      </nav>
     </div>
-  )
+  );
 }

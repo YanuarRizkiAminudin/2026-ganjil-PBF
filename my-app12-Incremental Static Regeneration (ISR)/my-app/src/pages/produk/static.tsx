fffdcd5ea18c018232618f5a8cbd111ac5f1e72c
Yanuar Rizki Aminudin <your-email@example.com>
2026-04-10 00:18:41 +0700

    perbaikan

--- my-app12-Incremental Static Regeneration (ISR)/my-app/src/pages/produk/static.tsx
+++ my-app12-Incremental Static Regeneration (ISR)/my-app/src/pages/produk/static.tsx
@@ -1,24 +1,21 @@
-// src/pages/produk/static.tsx
 import { ProductType } from '../../types/Product.type';
-import TampilanProduk from '../../../views/product';
+import TampilanProduk from '../../views/product';
 
 export default function StaticPage({ products }: { products: ProductType[] }) {
   return (
     <div>
-      <h1>Halaman Produk - Static Site Generation (SSG)</h1>
+      <h1>Halaman Produk SSG + ISR (revalidate 10 detik)</h1>
       <TampilanProduk products={products} />
     </div>
   );
 }
 
 export async function getStaticProps() {
-  // Panggil API internal (harus jalan saat build)
   const res = await fetch('http://localhost:3000/api/produk');
   const products = await res.json();
 
   return {
     props: { products },
-    // optional: ISR (revalidate setiap 10 detik)
-    revalidate: 10,
+    revalidate: 10, // 🔁 ISR: halaman akan diperbarui setiap 10 detik
   };
 }

45f85494fc3e690e3aadd8856060c9cafb0f0068
Yanuar Rizki Aminudin <your-email@example.com>
2026-04-24 03:11:26 +0700

    IMPLEMENTASI LOGIN GOOGLE PROVIDER DENGAN NEXTAUTH.JS + FIREBASE

--- my-app16- Sistem Autentikasi Login Terintegrasi Database - Copy/my-app/next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import("next").NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  serverExternalPackages: ["firebase", "firebase-admin"],
-  allowedDevOrigins: ["192.168.56.1"],
-}
-module.exports = nextConfig
---
+++ my-app17- Implementasi Login Google Provider dengan NextAuth.js tambah Firebase/my-app/next.config.js
@@ -0,0 +1,19 @@
+/** @type {import("next").NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  serverExternalPackages: ["firebase", "firebase-admin"],
+  allowedDevOrigins: ["192.168.56.1"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com", // avatar Google
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com", // avatar GitHub
+      },
+    ],
+  },
+}
+module.exports = nextConfig

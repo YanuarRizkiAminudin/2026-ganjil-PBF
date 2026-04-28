40c0199aa8d36e73179e34b16226b50285bb74c2
Yanuar Rizki Aminudin <your-email@example.com>
2026-04-28 08:48:55 +0700

    perbaikan uri reirect

--- my-app-deployment/my-app/next.config.js
+++ my-app-deployment/my-app/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import("next").NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL || "https://2026-ganjil-pbf.vercel.app",
+  },
   serverExternalPackages: ["firebase", "firebase-admin"],
   allowedDevOrigins: ["192.168.56.1"],
   images: {
